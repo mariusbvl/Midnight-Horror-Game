fe04a4ab23f6f473f5681fd98606ab4ba9786805
mariusbvl <mariusbivol2003@gmail.com>
2024-06-10 18:59:20 +0300

    Changed crouching in player controller

--- Assets/Scripts/FPC/FirstPersonController.cs
+++ Assets/Scripts/FPC/FirstPersonController.cs
@@ -1,37 +1,47 @@
+using System.Collections;
 using UnityEngine;
+
+
 namespace FPC
 {
     public class FirstPersonController : MonoBehaviour
     {
         private GameInputActions _inputActions;
-        [Header("Camera")]
-        [SerializeField] private Transform mainCamera;
-        [SerializeField] private Transform cameraStand;
-        [SerializeField] private Transform cameraCrouch;
-        [SerializeField] private float crouchingDuration = 2f;
-        [Header("Player")]
-        [SerializeField]private float moveSpeed = 6f;
-        [SerializeField] private float sprintSpeed = 10f;
-        private Vector3 _velocity;
-        private float _gravity = -9.81f;
-        private Vector2 _move;
-        private float _jumpHeight = 2f;
         private CharacterController _characterController;
-        public Transform ground;
-        public float distanceToGround = 0.4f;
-        public LayerMask groundMask;
-        public bool isGrounded;
-        public bool isSprinting;
-        public bool isCrouching;
+        private float _moveSpeed;
+        [Header("Move")]
+        [SerializeField] private float walkSpeed;
+        private Vector3 _velocity;
+        private Vector2 _move;
+        [Header("Jump")]
+        [SerializeField] private float gravity = -9.81f;
+        [SerializeField] private float jumpHeight = 2f;
+        [SerializeField] private Transform ground;
+        [SerializeField] private float distanceToGround = 0.4f;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private bool isGrounded;
+        [Header("Sprint")]
+        [SerializeField] private float sprintSpeed;
+        [SerializeField] private bool isSprinting;
+        [Header("Crouch")]
+        [SerializeField] private float crouchSpeed;
+        [SerializeField] private float crouchTime;
+        [SerializeField] private float standHeight;
+        [SerializeField] private float crouchHeight;
+        [SerializeField] private Vector3 crouchingCenter;
+        [SerializeField] private Vector3 standingCenter;
+        [SerializeField] private bool isCrouching;
 
         private void Awake()
         {
+            _moveSpeed = walkSpeed;
+            
             _inputActions = new GameInputActions();
             _characterController = GetComponent<CharacterController>();
 
-            _inputActions.Player.Sprint.performed += x => SprintdPressed();
-            _inputActions.Player.Sprint.canceled += x => SprintReleased();
-            _inputActions.Player.Crouch.performed += x => CrouchPressed();
+            _inputActions.Player.Sprint.performed += _ => SprintPressed();
+            _inputActions.Player.Sprint.canceled += _ => SprintReleased();
+            _inputActions.Player.Crouch.performed += _ => CrouchPressed();
         }
 
         private void Update()
@@ -51,15 +61,16 @@ namespace FPC
                 _velocity.y = -2f;
             }
 
-            _velocity.y += _gravity * Time.deltaTime;
+            _velocity.y += gravity * Time.deltaTime;
             _characterController.Move(_velocity * Time.deltaTime);
         }
 
         private void PlayerMovement()
         {
             _move = _inputActions.Player.Move.ReadValue<Vector2>();
-            Vector3 movement = (_move.y * transform.forward) + (_move.x * transform.right);
-            _characterController.Move(movement * moveSpeed * Time.deltaTime);
+            var transform1 = transform;
+            Vector3 movement = (_move.y * transform1.forward) + (_move.x * transform1.right);
+            _characterController.Move(movement * (_moveSpeed * Time.deltaTime));
         }
 
         private void Jump()
@@ -68,45 +79,71 @@ namespace FPC
             {
                 if (_inputActions.Player.Jump.triggered && isGrounded)
                 {
-                    _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+                    _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
                 }
             }
         }
 
         private void Sprint()
         {
-            if (isSprinting)
+            if (isSprinting && isGrounded)
             {
-                moveSpeed = sprintSpeed;
+                _moveSpeed = sprintSpeed;
             }
-            else
+            else if(!isSprinting && isGrounded)
             {
-                moveSpeed = 6f;
+                _moveSpeed = walkSpeed;
             }
         }
 
         private void Crouch()
         {
+            if (isGrounded)
+            {
+                if (_inputActions.Player.Crouch.triggered && isCrouching)
+                {
+                    StartCoroutine(CrouchStand());
+                }
+            }
+            if (_inputActions.Player.Crouch.triggered && !isCrouching)
+            {
+                StartCoroutine(CrouchStand());
+            }
+
             if (isCrouching)
             {
-                var center = _characterController.center;
-                center.y = -0.5f;
-                _characterController.height = 1f;
-                _characterController.center = center;
-                mainCamera.position = Vector3.Lerp(mainCamera.position, cameraCrouch.position, crouchingDuration * Time.deltaTime);
+                _moveSpeed = crouchSpeed;
             }
-            else if(!isCrouching && isGrounded)
+            else if (!isSprinting && isCrouching)
             {
-                var center = _characterController.center;
-                center.y = 0f;
-                _characterController.height = 2f;
-                _characterController.center = center;
-                mainCamera.position = Vector3.Lerp(mainCamera.position, cameraStand.position, crouchingDuration * Time.deltaTime);
+                _moveSpeed = walkSpeed;
             }
         }
-        private void SprintdPressed()
+        
+        private IEnumerator CrouchStand()
         {
-            isSprinting = true;
+            float timeElapsed = 0;
+            float targetHeight = isCrouching ? crouchHeight : standHeight;
+            float currentHeight = _characterController.height;
+            Vector3 targetCenter = isCrouching ? crouchingCenter : standingCenter;
+            Vector3 currentCenter = _characterController.center;
+            while (timeElapsed < crouchTime)
+            {
+                _characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed/crouchTime);
+                _characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed/crouchTime);
+                timeElapsed += Time.deltaTime;
+                yield return null; 
+            }
+            _characterController.height = targetHeight;
+            _characterController.center = targetCenter;
+        }
+        
+        private void SprintPressed()
+        {
+            if (isGrounded)
+            {
+                isSprinting = true;
+            }
         }
 
         private void SprintReleased()
