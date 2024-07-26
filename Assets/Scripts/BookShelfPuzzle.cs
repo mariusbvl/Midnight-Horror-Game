6b987530c5d770e413f377b282a18e67dbf06b55
mariusbvl <mariusbivol2003@gmail.com>
2024-07-26 15:07:35 +0300

    Aquired getting the correct books

--- Assets/Scripts/BookShelfPuzzle.cs
+++ Assets/Scripts/BookShelfPuzzle.cs
@@ -6,8 +6,10 @@ using Random = UnityEngine.Random;
 public class BookShelfPuzzle : MonoBehaviour
 {
    public static BookShelfPuzzle Instance { get; private set; }
-   [Header("General")]
+   [Header("General")] 
+   public bool canTryBook;
    public string correctCombination;
+   [SerializeField] private GameObject[] correctBooks;
    [SerializeField] private GameObject[] allBooks;
    [SerializeField] private GameObject[]  firstRowBooks;
    [SerializeField] private GameObject[]  secondRowBooks;
@@ -17,6 +19,7 @@ public class BookShelfPuzzle : MonoBehaviour
    private int _secondRowRandomIndex;
    private int _thirdRowRandomIndex;
    private int _fourthRowRandomIndex;
+   [SerializeField]private bool[] correctBooksBool;
    [Header("TryBook")] 
    [SerializeField] private float tryDuration;
    [SerializeField] private float returnDuration;
@@ -26,18 +29,35 @@ public class BookShelfPuzzle : MonoBehaviour
       {
          Instance = this;
       }
+      canTryBook = true;
       _firstRowRandomIndex = Random.Range(0, firstRowBooks.Length);
       _secondRowRandomIndex = Random.Range(0, secondRowBooks.Length);
       _thirdRowRandomIndex = Random.Range(0, thirdRowBooks.Length);
       _fourthRowRandomIndex = Random.Range(0, fourthRowBooks.Length);
-      correctCombination = $"{_firstRowRandomIndex} {_secondRowRandomIndex} {_thirdRowRandomIndex} {_fourthRowRandomIndex}";
+      correctCombination = $"{_firstRowRandomIndex + 1} {_secondRowRandomIndex + 1} {_thirdRowRandomIndex + 1} {_fourthRowRandomIndex + 1}";
+      correctBooks[0] = firstRowBooks[_firstRowRandomIndex];
+      correctBooks[1] = secondRowBooks[_secondRowRandomIndex];
+      correctBooks[2] = thirdRowBooks[_thirdRowRandomIndex];
+      correctBooks[3] = fourthRowBooks[_fourthRowRandomIndex];
    }
 
    public IEnumerator InteractWithBook()
    {
+      if(!canTryBook) yield break;
+      GameObject book = InteractController.Instance.bookParent;
       yield return TryBook();
-      yield return new WaitForSeconds(1f);
-      yield return ReturnBook();
+      if (!CheckBook(book))
+      {
+         ReturnAllBooks();
+         ResetCorrectBookArray();
+         yield return new WaitForSeconds(0.5f);
+         yield return ReturnBook();
+      }
+      if(AreAllCorrectBook())
+      {
+         Debug.Log("All books activated");
+         canTryBook = false;
+      }
    }
 
    private IEnumerator TryBook()
@@ -45,7 +65,8 @@ public class BookShelfPuzzle : MonoBehaviour
       if(InteractController.Instance.bookPivot.transform.rotation != InteractController.Instance.bookInitialPivot.transform.rotation) yield break;
       Quaternion startRotation = InteractController.Instance.bookInitialPivot.transform.rotation;
       Quaternion targetRotation = InteractController.Instance.bookTargetPivot.transform.rotation;
-
+      
+      
       float timeElapsed = 0;
       while (timeElapsed < tryDuration)
       {
@@ -58,7 +79,7 @@ public class BookShelfPuzzle : MonoBehaviour
 
    private IEnumerator ReturnBook()
    {
-      
+      if(InteractController.Instance.bookPivot.transform.rotation != InteractController.Instance.bookTargetPivot.transform.rotation) yield break;
       Quaternion startRotation = InteractController.Instance.bookTargetPivot.transform.rotation;
       Quaternion targetRotation = InteractController.Instance.bookInitialPivot.transform.rotation;
 
@@ -70,5 +91,93 @@ public class BookShelfPuzzle : MonoBehaviour
          yield return null;
       }
       InteractController.Instance.bookPivot.transform.rotation = targetRotation;
+   }
+      
+   private IEnumerator ReturnBook(GameObject bookParent, Transform bookPivot)
+   {
+      
+      Quaternion startRotation = bookParent.transform.Find("targetPivot").transform.rotation;
+      Quaternion targetRotation = bookParent.transform.Find("initialPivot").transform.rotation;
+
+      float timeElapsed = 0;
+      while (timeElapsed < returnDuration)
+      {
+         bookPivot.transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / returnDuration);
+         timeElapsed += Time.deltaTime; 
+         yield return null;
+      }
+      bookPivot.transform.rotation = targetRotation;
+   }
+   private void ReturnAllBooks()
+   {
+      if(!AreAnyCorrectBooks()) return;
+      for (int i = 0; i < allBooks.Length; i++)
+      {
+         GameObject bookParent = allBooks[i].gameObject;
+         GameObject book = bookParent.GetComponentInChildren<BoxCollider>().gameObject;
+         Transform bookPivot = book.transform.parent.gameObject.transform;
+         Transform initialPivot = bookParent.transform.Find("initialPivot");
+         if (bookPivot.transform.rotation != initialPivot.transform.rotation)
+         {
+            StartCoroutine(ReturnBook(bookParent, bookPivot));
+         }
+      }
+   }
+   
+   private bool CheckBook(GameObject book)
+   {
+      foreach (GameObject b in correctBooks)
+      {
+         if (b == book)
+         {
+            AddCorrectBook();
+            return true;
+         }
+      }
+      return false;
+   }
+
+   private void AddCorrectBook()
+   {
+      for (int i = 0; i < correctBooksBool.Length; i++)
+      {
+         if (!correctBooksBool[i])
+         {
+            correctBooksBool[i] = true;
+            break;
+         }
+      }
+   }
+   
+   private void ResetCorrectBookArray()
+   {
+      for (int i = 0; i < correctBooksBool.Length; i++)
+      {
+         correctBooksBool[i] = false;
+      }
+   }
+
+   private bool AreAnyCorrectBooks()
+   {
+      for (int i = 0; i < correctBooksBool.Length; i++)
+      {
+         if (correctBooksBool[i])
+         {
+            return true;
+         }
+      }
+      return false;
+   }
+   
+   private bool AreAllCorrectBook()
+   {
+      for (int i = 0; i < correctBooksBool.Length; i++)
+      {
+         if (!correctBooksBool[i])
+         {
+            return false;
+         }
+      }
+      return true;
    }
 }
