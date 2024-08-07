3d0de68f01b428290f88cebba88d1e8e5c72031f
mariusbvl <mariusbivol2003@gmail.com>
2024-08-07 17:35:23 +0300

    Added option panel and confirmation panel

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,27 +1,70 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private GameInputActions _inputActions;
     [SerializeField] private Button startButton;
-
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject blurredPanel;
+    [SerializeField] private GameObject mainObjectsPanel;
+    [SerializeField] private Slider cameraSensitivitySlider;
+    [SerializeField] private GameObject confirmationPanel;
+    [SerializeField] private Button noButton;
+    [SerializeField] private Button yesButton;
     private void Awake()
     {
+        _inputActions = new GameInputActions();
         EventSystem.current.SetSelectedGameObject(startButton.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenExitConfirmationPanel()
     {
-        
+        mainObjectsPanel.SetActive(false);
+        confirmationPanel.SetActive(true);
+        blurredPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(noButton.gameObject);
+    }
+    
+    public void CloseExitConfirmationPanel()
+    {
+        mainObjectsPanel.SetActive(true);
+        confirmationPanel.SetActive(false);
+        blurredPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(exitButton.gameObject);
     }
     public void ExitGame()
     {
         Debug.Log("Exited game");
         Application.Quit();    
+    }
+
+    public void OpenOptionsPanel()
+    {
+        mainObjectsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+        blurredPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(cameraSensitivitySlider.gameObject);
+    }
+    
+    public void CloseOptionsPanel()
+    {
+        optionsPanel.SetActive(false);
+        blurredPanel.SetActive(false);
+        mainObjectsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(optionsButton.gameObject);
+    }
+    
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
     }
 }
